192ab08547d138e6cb3a183313a636034af78c18
Glenda <glenda_wee@mymail.sutd.edu.sg>
2021-04-05 02:17:39 +0800

    sit.py update

--- data-collection/sit.py
+++ data-collection/sit.py
@@ -16,12 +16,13 @@ def take_ten_frames(mlx, frame):
             mlx.getFrame(frame)
         except ValueError:
             continue
+        print(np.shape(frame))
+        print(np.shape(frames))
         print(frame)
         frame = np.array(frame)
         # frame_formatted = np.reshape(frame,(24, 32)) 
         frames = np.vstack((frames, frame))
-        print(np.shape(frame))
-        print(np.shape(frames))
+        
         print("frame", (i + 1), " captured")
         
     
