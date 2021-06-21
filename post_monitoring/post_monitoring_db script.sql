7771a1117797b905a072cd8e1d5e76286f3be642
Hui Ting <ganhuiting98@gmail.com>
2021-06-21 12:00:37 +0800

    Changed Schema Data Type

--- post_monitoring/post_monitoring_db script.sql
+++ post_monitoring/post_monitoring_db script.sql
@@ -2,16 +2,16 @@ CREATE SCHEMA `post_monitoring_db` ;
 
 CREATE TABLE `post_monitoring_db`.`current_patient_logs`(
   `bed_number` INT NULL,
-  `timestamp_start` TIMESTAMP NULL,
-  `timestamp_end`  TIMESTAMP NULL,
+  `timestamp_start` DATETIME NULL,
+  `timestamp_end` DATETIME NULL,
   `accompanied` INT NULL,
   `hfr_count` INT NULL);
 
 
 CREATE TABLE `post_monitoring_db`.`discharged_patient_logs` (
   `bed_number` INT NULL,
-  `timestamp_start` TIMESTAMP NULL,
-  `timestamp_end`  TIMESTAMP NULL,
+  `timestamp_start` DATETIME NULL,
+  `timestamp_end` DATETIME NULL,
   `accompanied` INT NULL,
   `hfr_count` INT NULL,
   `date_first` DATE NULL,
